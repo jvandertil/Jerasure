7d290a4e640b7bedfd14b55cd1415ae383f1c117
Jos van der Til <jos@vandertil.eu>
2013-03-01 21:45:18 +0100

    Updated Java sources

--- java/src/eu/vandertil/jerasure/Decoding.java
+++ java/src/eu/vandertil/jerasure/Decoding.java
@@ -1,27 +1,12 @@
-package eu.vandertil.jerasure.jni;
-
+package eu.vandertil.jerasure;
 /**
  * Created with IntelliJ IDEA.
  * User: Jos
- * Date: 28-2-13
- * Time: 14:07
+ * Date: 1-3-13
+ * Time: 13:46
  * To change this template use File | Settings | File Templates.
  */
 public class Decoding {
-    public static native boolean decodeWithMatrix(int numDataDevices, int numCodingDevices, int wordSize, int[] matrix,
-                                                  boolean firstRowInMatrixIsAllOnes, int[] erasedDevices,
-                                                  byte[][] dataDevices, byte[][] codingDevices, int size);
-
-    /**
-     *
-     * @param matrix The coding matrix
-     * @param deviceStatus The status of each device. If a device is working it should be true, otherwise false.
-     *                     An id between 0 and dataDevices.length represents a data device.
-     *                     An id between dataDevices.length and (codingDevices.length + dataDevices.length) represents a coding device.
-     * @param dataDevices The data devices
-     * @param codingDevices The coding devices
-     * @return
-     */
     public static boolean decodeWithMatrix(int[] matrix, boolean[] deviceStatus, byte[][] dataDevices, byte[][] codingDevices) {
         int expectedArrayLength = dataDevices[0].length;
         for (int i = 0; i < dataDevices.length; ++i) {
@@ -58,8 +43,9 @@ public class Decoding {
 
         erasedDevices[index] = -1;
 
-        return decodeWithMatrix(numDataDevices, numCodingDevices, 8, matrix, firstRowInMatrixIsAllOnes,
-                                erasedDevices, dataDevices, codingDevices, expectedArrayLength);
+        /*return eu.vandertil.jerasure.jni.Decoding.decodeWithMatrix(numDataDevices, numCodingDevices, 8, matrix, firstRowInMatrixIsAllOnes,
+                erasedDevices, dataDevices, codingDevices, expectedArrayLength);
+        */
+        return true;
     }
-
 }
--- java/src/eu/vandertil/jerasure/jni/ReedSolomon.java
+++ java/src/eu/vandertil/jerasure/jni/ReedSolomon.java
@@ -1,55 +1,23 @@
 package eu.vandertil.jerasure.jni;
 
 /**
- * This class provides JNI coupling to the JErasure ReedSolomon encoding.
+ * This class provides access to the functions described in reed_sol.h.
  *
- * @author Jos van der Til (JNI coupling)
- * @author James S. Plank (JErasure library)
+ * See the documentation of Jerasure for a description of functionality and parameters.
+ *
+ * @author Jos van der Til
  * @version 1.0
  * @since 1.0
  */
 public class ReedSolomon {
-    /**
-     * This returns the last m rows of the distribution matrix
-     * in GF(2^w), based on an extended Vandermonde matrix.
-     * The first row of this matrix is guaranteed to be all ones. The first column is also
-     * guaranteed to be all ones.
-     *
-     * @param dataDevices   The number of data devices.
-     * @param codingDevices The number of coding devices.
-     * @param wordSize      The word size of the data (byte = 8 / short = 16 / int = 32 / etc)
-     *
-     * @return A m * k matrix that can be used with the matrix routines in Jerasure.
-     */
-    public static native int[] getVandermondeCodingMatrix(int dataDevices, int codingDevices, int wordSize);
+    public static native int[] reed_sol_vandermonde_coding_matrix(int k, int m, int w);
+    public static native int[] reed_sol_extended_vandermonde_matrix(int rows, int cols, int w);
+    public static native int[] reed_sol_big_vandermonde_distribution_matrix(int rows, int cols, int w);
 
-    /**
-     * Convenience method for getVandermondeCodingMatrix with wordsize 8.
-     */
-    public static int[] getVandermondeCodingMatrixForBytes(int dataDevices, int codingDevices) {
-        return getVandermondeCodingMatrix(dataDevices, codingDevices, 8);
-    }
+    public static native boolean reed_sol_r6_encode(int k, int w, byte[][] data_ptrs, byte[][] coding_ptrs, int size);
+    public static native int[] reed_sol_r6_coding_matrix(int k, int w);
 
-    /**
-     * This creates an extended Vandermonde matrix with rows rows and cols columns in GF(2^w).
-     *
-     * @param rows     The requested number of rows
-     * @param cols     The requested number of columns
-     * @param wordSize The word size.
-     *
-     * @return an extended Vandermonde matrix with 'rows' rows and 'cols' columns in GF(2^w).
-     */
-    public static native int[] getExtendedVandermondeMatrix(int rows, int cols, int wordSize);
-
-    /**
-     * This converts the extended matrix from getExtendedVandermondeMatrix into a distribution matrix so that the top 'cols' rows compose an identity matrix,
-     * and the remaining rows are in the format returned by getVandermondeCodingMatrix.
-     *
-     * @param rows     The requested number of rows
-     * @param cols     The requested number of columns
-     * @param wordSize The word size.
-     *
-     * @return
-     */
-    public static native int[] getBigVandermondeDistributionMatrix(int rows, int cols, int wordSize);
+    public static native void reed_sol_galois_w08_region_multby_2(byte[] region, int nbytes);
+    public static native void reed_sol_galois_w16_region_multby_2(byte[] region, int nbytes);
+    public static native void reed_sol_galois_w32_region_multby_2(byte[] region, int nbytes);
 }
